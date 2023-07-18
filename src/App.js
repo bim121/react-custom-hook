acd1b6590c62cd6914c90add2d8a38ae1651de48
bim121 <vitisik54@gmail.com>
2023-07-18 16:32:00 +0300

    useEventListener

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import ArrayComponent from './componentExample/ArrayComponent';
 import AsyncComponent from './componentExample/AsyncComponent';
+import EventListenerComponent from './componentExample/EventListenerComponent';
 
 function App() {
   return (
     <div className="App">
-      <AsyncComponent />
+      <EventListenerComponent />
     </div>
   );
 }
