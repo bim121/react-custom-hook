2a7c2779e4f7cba59a5f2ce51633d996a50d757e
bim121 <vitisik54@gmail.com>
2023-07-18 17:02:15 +0300

    useClickOutside

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import ArrayComponent from './componentExample/ArrayComponent';
 import AsyncComponent from './componentExample/AsyncComponent';
+import ClickOutsideComponent from './componentExample/ClickOutsideComponent';
 import EventListenerComponent from './componentExample/EventListenerComponent';
 
 function App() {
   return (
     <div className="App">
-      <EventListenerComponent />
+      <ClickOutsideComponent/>
     </div>
   );
 }
--- src/customHooks/useEventListener.js
+++ src/customHooks/useEventListener.js
@@ -6,12 +6,16 @@ export default function useEventListener(
     element = window
 ){
     const callbackRef = useRef(callback)
+
     useEffect(() => {
         callbackRef.current = callback
-    }, callback)
+    }, [callback])
+
     useEffect(() => {
         if(element == null) return
+
         const handler = e => callbackRef.current(e)
+
         element.addEventListener(eventType, handler)
         return () => element.removeEventListener(eventType, handler)
     }, [eventType, element])
